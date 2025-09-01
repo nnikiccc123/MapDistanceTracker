516349960225b4ce8ecfa44156ede42e4d7a90eb
Natalija Nikic <nnikiccc123@gmail.com>
2025-09-02 01:10:19 +0200

    dodavanje mogucnosti double klina ka mobilnoj verz

--- static/js/script.js
+++ static/js/script.js
@@ -2,18 +2,14 @@ let initMap = () => {
     let map = L.map('map', { attributionControl: false, doubleClickZoom: false }).setView([44.80, 20.45], 13);
 
     L.tileLayer('https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i349018013!3m9!2sen-US!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0',
-        {
-            maxZoom: 19,
-        }).addTo(map);
-
+        { maxZoom: 19 }).addTo(map);
 
     let markers = [];
     let polylines = [];
     let allObjects = [];
 
     let createMarker = (popLocation) => {
-        let marker = L.circleMarker(popLocation);
-        marker.setStyle({color: 'red', fillColor: 'red', fillOpacity: 1.0, radius: 3});
+        let marker = L.circleMarker(popLocation, { color: 'red', fillColor: 'red', fillOpacity: 1.0, radius: 3 });
         markers.push(marker);
         allObjects.push(marker);
         return marker;
@@ -31,12 +27,12 @@ let initMap = () => {
     }
 
     let createTooltip = () => {
-        let tooltip = L.tooltip({offset: L.point(5, 0), permanent: true});
+        let tooltip = L.tooltip({ offset: L.point(5, 0), permanent: true });
         allObjects.push(tooltip);
         return tooltip;
     }
 
-    clearAll = () => {
+    let clearAll = () => {
         allObjects.forEach(o => o.remove());
         allObjects = [];
         markers = [];
@@ -44,9 +40,7 @@ let initMap = () => {
     }
 
     let calcDistance = (markers) => {
-        if (markers.length < 2) {
-            return "";
-        }
+        if (markers.length < 2) return "";
         let totalDistance = 0;
         for (let i = 0; i < markers.length - 1; i++) {
             totalDistance += markers[i].getLatLng().distanceTo(markers[i + 1].getLatLng());
@@ -59,48 +53,53 @@ let initMap = () => {
     }
 
     let removeLastPoint = () => {
-        if (markers.length > 0) {
-            markers.pop().remove();
-        }
-        if (polylines.length > 0) {
-            polylines.pop().remove();
-        }
+        if (markers.length > 0) markers.pop().remove();
+        if (polylines.length > 0) polylines.pop().remove();
         updateLastDistance();
     }
 
-    map.on('dblclick', e => {
+    let finishPath = () => {
         if (markers.length > 1) {
-            markers[markers.length - 1].addTo(map);
+            let lastLocation = markers[markers.length - 1].getLatLng();
+            createTooltip().setLatLng(lastLocation)
+                           .setContent(`<div class="maptooltip">${calcDistance(markers)}</div>`)
+                           .addTo(map);
         }
-
-        let popLocation = e.latlng;
-        createTooltip().setLatLng(popLocation).setContent(`<div class="maptooltip">${calcDistance(markers)}</div>`).addTo(map);
-
         markers = [];
         polylines = [];
-
         updateLastDistance();
+    }
+
+    // Desktop dblclick
+    map.on('dblclick', e => {
+        finishPath();
+    });
+
+    // Simulacija double-tap na mobilnim uređajima
+    let lastTap = 0;
+    map.on('touchend', e => {
+        let currentTime = new Date().getTime();
+        let tapLength = currentTime - lastTap;
+        if (tapLength < 500 && tapLength > 0) { // dvoklik ako su dodiri < 500ms
+            finishPath();
+        }
+        lastTap = currentTime;
     });
 
     map.on('click', e => {
         let popLocation = e.latlng;
         let marker = createMarker(popLocation);
-        if (markers.length === 1) {
-            marker.addTo(map);
-        }
+        if (markers.length === 1) marker.addTo(map);
         if (markers.length >= 2) {
             createPolyline();
-            document.getElementById("total-distance").innerHTML = calcDistance(markers);
+            updateLastDistance();
         }
     });
 
-    map.on('contextmenu', e => {
-        removeLastPoint();
-    });
+    map.on('contextmenu', e => removeLastPoint());
 
     document.getElementById('clear').addEventListener('click', function () {
         clearAll();
         updateLastDistance();
-
     });
 };
